a2ce9727aea3e74485958b9b97899e4795da0146
Xiao Wu <923071808@qq.com>
2022-07-06 21:25:25 +0800

    Update option_bdpn.py

--- UDL/pansharpening/configs/option_bdpn.py
+++ UDL/pansharpening/configs/option_bdpn.py
@@ -13,7 +13,7 @@ class parser_args(TaskDispatcher, name='BDPN'):
         script_path = os.path.dirname(os.path.dirname(__file__))
         root_dir = script_path.split(cfg.task)[0]
 
-        model_path = f'{root_dir}/results/{cfg.task}/wv3/BDPN/Test/model_2022-05-09-21-02-40/900.pth.tar'
+        model_path = f'{root_dir}/results/{cfg.task}/wv3/BDPN/Test/.pth.tar'
 
         parser = argparse.ArgumentParser(description='PyTorch Pansharpening Training')
         # * Logger
@@ -35,10 +35,9 @@ class parser_args(TaskDispatcher, name='BDPN'):
         # * Model and Dataset
         parser.add_argument('--arch', '-a', metavar='ARCH', default='BDPN', type=str,
                             choices=['PanNet', 'DiCNN', 'PNN', 'FusionNet'])
-        parser.add_argument('--dataset', default={'train': 'wv3', 'val': 'wv3_multiExm.h5'}, type=str,
+        parser.add_argument('--dataset', default={'train': 'wv3', 'val': 'wv3_multiExm1.h5'}, type=str,
                             choices=[None, 'wv2', 'wv3', 'wv4', 'qb', 'gf',
-                                     'wv2_hp', ...,
-                                     'fr', 'wv3_singleMat', 'wv3_multi_exm1258', 'wv3_multiExm1.h5'],
+                                     'wv3_OrigScale_multiExm1.h5', 'wv3_multiExm1.h5', ...],
                             help="performing evalution for patch2entire")
         parser.add_argument('--eval', default=False, type=bool,
                             help="performing evalution for patch2entire")
