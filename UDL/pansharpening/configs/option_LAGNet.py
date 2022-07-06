f9d171593ed6bd4f30f90adb64ca47fcfb987620
Xiao Wu <923071808@qq.com>
2022-07-06 21:24:09 +0800

    Update option_LAGNet.py

--- UDL/pansharpening/configs/option_LAGNet.py
+++ UDL/pansharpening/configs/option_LAGNet.py
@@ -13,9 +13,7 @@ class parser_args(TaskDispatcher, name='LAGNet'):
         script_path = os.path.dirname(os.path.dirname(__file__))
         root_dir = script_path.split(cfg.task)[0]
 
-        # model_path = f'/home/woo/文档/GitHub/UDL_bak/UDL/results/pansharpening/gf2/LAGNet/Test/model_2022-05-10-15-01-46/999.pth.tar'
-        model_path = f'/home/woo/文档/GitHub/UDL_bak/UDL/results/pansharpening/wv3/LAGNet/Test/model_2022-04-21-00-35-31/990.pth.tar'
-        # model_path = f'/home/woo/文档/GitHub/UDL_bak/UDL/results/pansharpening/qb/LAGNet/Test/model_2022-04-22-13-23-09/990.pth.tar'
+        model_path = f'.pth.tar'
         parser = argparse.ArgumentParser(description='PyTorch Pansharpening Training')
         # * Logger
         parser.add_argument('--out_dir', metavar='DIR', default=f'{root_dir}/results/{cfg.task}',
@@ -38,10 +36,9 @@ class parser_args(TaskDispatcher, name='LAGNet'):
                             help='path to latest checkpoint (default: none)')
         ##
         parser.add_argument('--arch', '-a', metavar='ARCH', default='LAGNet', type=str)
-        parser.add_argument('--dataset', default={'train': 'wv3', 'val': 'wv3_multiExm.h5'}, type=str,
+        parser.add_argument('--dataset', default={'train': 'wv3', 'val': 'wv3_multiExm1.h5'}, type=str,
                             choices=[None, 'wv2', 'wv3', 'wv4', 'qb', 'gf',
-                                     'wv2_hp', ...,
-                                     'fr', 'wv3_singleMat', 'wv3_multi_exm1258'],
+                                     'wv2_hp', ..., 'wv3_OrigScale_multiExm1.h5', 'wv3_multiExm1.h5'],
                             help="performing evalution for patch2entire")
         parser.add_argument('--eval', default=False, type=bool,
                             help="performing evalution for patch2entire")
