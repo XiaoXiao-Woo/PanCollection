34a3bf4f902d17c41efa762425899e7e33a9e0b1
XiaoXiao-woo <923071808@qq.com>
2023-03-22 20:04:34 +0800

    delete files

--- UDL/pansharpening/configs/option_bdpn.py
+++ UDL/pansharpening/configs/option_bdpn.py
@@ -5,7 +5,7 @@ import os
 
 class parser_args(TaskDispatcher, name='BDPN'):
     def __init__(self, cfg=None):
-
+        super(parser_args, self).__init__()
         if cfg is None:
             from UDL.Basis.option import panshaprening_cfg
             cfg = panshaprening_cfg()
@@ -53,5 +53,5 @@ class parser_args(TaskDispatcher, name='BDPN'):
         cfg.workflow = [('train', 50), ('val', 1)]
         # cfg.workflow = [('val', 1)]
         # cfg.workflow = [('train', 50)]
-        self._cfg_dict = cfg
+        self.merge_from_dict(cfg)
 
