6994951df6b23b7510306d672896be5f9700f746
XiaoXiao-woo <923071808@qq.com>
2023-03-27 14:17:56 +0800

    Update run_test_pansharpening.py

--- run_test_pansharpening.py
+++ run_test_pansharpening.py
@@ -1,10 +1,11 @@
-from UDL.AutoDL import TaskDispatcher
+from configs.configs import TaskDispatcher
 from UDL.AutoDL.trainer import main
+from common.builder import build_model, getDataSession
 
 if __name__ == '__main__':
     # cfg = parser_args()
-    cfg = TaskDispatcher.new(task='pansharpening', mode='entrypoint', arch='DCFNet')
+    cfg = TaskDispatcher.new(task='pansharpening', mode='entrypoint', arch='BDPN')
     cfg.eval = True  # perform test on the dataset of wv3_multiExm.h5.
-    # cfg.dataset = {'train': 'wv3', 'val': 'wv3_multiExm.h5'} #'wv3_OrigScale_multiExm1.h5'
+    # cfg.dataset = {'train': 'wv3', 'valid': 'wv3_multiExm.h5'} #'wv3_OrigScale_multiExm1.h5'
     print(TaskDispatcher._task.keys())
-    main(cfg)
+    main(cfg, build_model, getDataSession)
