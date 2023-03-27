b5e3a210d63aba5b6e3e3fe4b4c2f1a1583da559
Xiao Wu <923071808@qq.com>
2023-03-27 14:18:23 +0800

    Update run_test_pansharpening.py

--- UDL/pansharpening/run_test_pansharpening.py
+++ UDL/pansharpening/run_test_pansharpening.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     # cfg = parser_args()
     cfg = TaskDispatcher.new(task='pansharpening', mode='entrypoint', arch='BDPN')
     cfg.eval = True  # perform test on the dataset of wv3_multiExm.h5.
-    # cfg.dataset = {'train': 'wv3', 'val': 'wv3_multiExm.h5'} #'wv3_OrigScale_multiExm1.h5'
+    # cfg.dataset = {'train': 'wv3', 'valid': 'wv3_multiExm.h5'} #'wv3_OrigScale_multiExm1.h5'
     print(TaskDispatcher._task.keys())
     main(cfg)
